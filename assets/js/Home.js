af473ac01365e735c43db4b1763574efe10c1de9
Kiritozhenggang <127064719+Kiritozhenggang@users.noreply.github.com>
2023-04-27 15:14:46 +0800

    Update Home.js

--- assets/js/Home.js
+++ assets/js/Home.js
@@ -1,10 +1,10 @@
 
-var input=prompt("Please choose your content 'student'or'teacher'")
+var input=prompt("Please choose your content 'student'or'parent'")
     if (input == "student"){
      window.location.href="https://kiritozhenggang.github.io/ICT441a/" 
     }
     
-    else if (input == "student")
+    else if (input == "parent")
     {
         window.location.href="https://kiritozhenggang.github.io/ICT441a/Course.html" 
     }

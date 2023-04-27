27c5cc3e4c74bf78d8c6b841803b17c4c630d674
Kiritozhenggang <127064719+Kiritozhenggang@users.noreply.github.com>
2023-04-27 15:04:44 +0800

    Add files via upload

---
+++ assets/js/Home.js
@@ -0,0 +1,10 @@
+
+var input=prompt("Please choose your content 'student'or'teacher'")
+    if (input == "student"){
+     window.location.href="https://kiritozhenggang.github.io/ICT441a/" 
+    }
+    
+    else if (input == "student")
+    {
+        window.location.href="https://kiritozhenggang.github.io/ICT441a/Course.html" 
+    }
